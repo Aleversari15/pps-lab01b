831a377c5539c46d38eccd23cc099850916c3f72
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2025-03-06 18:38:05 +0100

    Added some test for moves not allowed

--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -1,8 +1,7 @@
 package e2;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
   private Logics l;
@@ -19,16 +18,42 @@ public class LogicTest {
   }
 
   @Test
-  public void testMoveNotAllowed(){
-    assertFalse(l.hit(2,2));
-    assertTrue(l.hasKnight(INITIAL_KNIGHT_POS.getX(),INITIAL_KNIGHT_POS.getY())); //k pos not changed
+  public void testMoveNotAllowed1(){
+    Pair<Integer,Integer> wrongPos = new Pair<>(2, 2);
+    assertAll(
+            () ->  assertFalse(l.hit(wrongPos.getX(),wrongPos.getY())),
+            () -> assertTrue(l.hasKnight(INITIAL_KNIGHT_POS.getX(),INITIAL_KNIGHT_POS.getY())) //k pos not changed
+    );
   }
+
+  @Test
+  public void testMoveNotAllowed2(){
+    Pair<Integer,Integer> wrongPos = new Pair<>(0, 1);
+    assertAll(
+            () ->  assertFalse(l.hit(wrongPos.getX(),wrongPos.getY())),
+            () -> assertTrue(l.hasKnight(INITIAL_KNIGHT_POS.getX(),INITIAL_KNIGHT_POS.getY())) //k pos not changed
+    );
+  }
+
+  @Test
+  public void testMoveNotAllowed3(){
+    Pair<Integer,Integer> wrongPos = new Pair<>(1, 1);
+    assertAll(
+            () ->  assertFalse(l.hit(wrongPos.getX(),wrongPos.getY())),
+            () -> assertTrue(l.hasKnight(INITIAL_KNIGHT_POS.getX(),INITIAL_KNIGHT_POS.getY())) //k pos not changed
+    );
+  }
+
+  @Test
+  public void testMoveOutOfBounds() {
+    assertThrows(IndexOutOfBoundsException.class, () -> l.hit(-1, -1));
+    assertThrows(IndexOutOfBoundsException.class, () -> l.hit(5, 5));
+  }
+
 
   @Test
   public void testMoveAllowed(){
     l.hit(2,1); //k pos not changed
     assertFalse(l.hasKnight(INITIAL_KNIGHT_POS.getX(),INITIAL_KNIGHT_POS.getY())); //k pos should be changed
   }
-
-
 }
